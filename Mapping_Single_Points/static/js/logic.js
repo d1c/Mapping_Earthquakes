8edf0a7548e1e300dedb327af074d990f48cb486
David Clark <kc0osh@gmail.com>
2022-09-24 10:37:23 -0500

    Added single points to map.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,16 +3,32 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
+    center: [ 40.7, -94.5],
     zoom: 4
 });
 
+// Add a marker to the map for Los Angeles, CA
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle marker to the map. Radius is measured in meters.
+let circle = L.circle([34.0522, -118.2437], {
+  color: 'red',
+  radius: 300
+}).addTo(map);
+
+
+// Add a circleMarker to the map. Default radius is 10 pixels.
+let circleMarker = L.circleMarker([34.0522, -118.2437], {
+  color: 'black',
+  fillColor: '#ffffa1'
+}).addTo(map);
+
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    id: "dark-v10",
     accessToken: API_KEY
 });
 
