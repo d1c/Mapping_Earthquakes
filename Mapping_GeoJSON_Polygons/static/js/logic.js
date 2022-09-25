925eb5c40a120743be11973b482b53e9bfd3fd1a
David Clark <kc0osh@gmail.com>
2022-09-25 15:20:15 -0500

    Completed GeoJSON Polygons updates.

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -34,24 +34,23 @@ let map = L.map("mapid", {
 L.control.layers(baseMaps).addTo(map);
 
 // Add GeoJSON data.
-let torontoData = "https://raw.githubusercontent.com/d1c/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/d1c/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  color: "lightblue",
+  weight: 2,
+  fillColor: "yellow"
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-  console.log(data)
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
     style: myStyle,
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr> <h3>Destination: "
-                      + feature.properties.dst + "</h3>");
+      layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>")
     }
-  })
-  .addTo(map);
+  }).addTo(map);
 });
